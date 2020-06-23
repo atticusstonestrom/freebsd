5f99d9fcdc329b3652cd743f3943183f937d34f0
atticusstonestrom <atticusstonestrom@yahoo.com>
2020-06-23 15:20:19 -0400

    Update vtp_test.c

--- vtp_test.c
+++ vtp_test.c
@@ -6,27 +6,29 @@
 #include <sys/module.h>
 
 int main(int argc, char **argv) {
-        int syscall_num;
-        int modid;
-        struct module_stat stat;
-        if(argc!=2) {
-                printf("usage:\t%s <virtual address to resolve>\n", argv[0]);
-                exit(0); }
-        stat.version=sizeof(stat);
-        if((modid=modfind("sys/vtp"))==-1) {
-                perror("fatal in modfind");
-                exit(-1); }
-        if(modstat(modid, &stat)==-1) {
-                perror("fatal in modstat");
-                exit(-1); }
-        syscall_num=stat.data.intval;
-        printf("%d\n", syscall_num);
-        return syscall(syscall_num, argv[1]); }
-        /*if((modid=modfind("hello"))==-1) {
-                perror("fatal in modfind");
-                exit(-1); }
-        printf("%d\n", modid);
-        if(modstat(modid, &stat)==-1) {
-                perror("fatal in modstat");
-                exit(-1); }
-        return syscall(210, argv[1]); }*/
+	unsigned long vaddr;
+	if(argc!=2) {
+		printf("usage:\t%s <virtual address to resolve>\n", argv[0]);
+		exit(0); }
+	vaddr=strtoul(argv[1], NULL, 16);
+	unsigned long paddr=0;
+
+	int syscall_num;
+	int modid;
+	struct module_stat stat;
+	stat.version=sizeof(stat);
+	if((modid=modfind("sys/vtp"))==-1) {
+		perror("fatal in modfind");
+		exit(-1); }
+	if(modstat(modid, &stat)==-1) {
+		perror("fatal in modstat");
+		exit(-1); }
+	syscall_num=stat.data.intval;
+	
+	if(syscall(syscall_num, vaddr, &paddr)) {
+		perror("fatal in syscall");
+		exit(-1); }
+	printf("virtual address:	%p\n"
+	       "physical address:	%p\n",
+	       (void *)vaddr, (void *)paddr);
+	return 0; }

26eab34cabfb570f612900bc866e3ed85559babb
atticusstonestrom <atticusstonestrom@yahoo.com>
2020-06-23 22:08:10 -0400

    Update vtp.c

--- vtp.c
+++ vtp.c
@@ -6,6 +6,10 @@
 #include <sys/kernel.h>
 #include <sys/sysproto.h>
 #include <sys/systm.h>
+/*struct pmap_statistics {
+	int x; };
+#include <machine/pmap.h>
+#include <machine/vmparam.h>*/
 
 struct vtp_args {
 	unsigned long vaddr;
@@ -28,12 +32,13 @@ struct vtp_args {
 /////////////////////////////////////////////////////
 
 /////////////////////////////////////////////////////
-#define	DMAP_MIN_ADDRESS	(0xfffffd0000000000UL)
+#define	DMAP_MIN_ADDRESS	(0xfffff80000000000)
 #define PHYS_TO_VIRT(x) ((x)|DMAP_MIN_ADDRESS)
 /////////////////////////////////////////////////////
 
 static int
 vtp(struct thread *td, void *args) {
+	//uprintf("[debug]: DMAP_MIN_ADDRESS: 0x%lx\n", DMAP_MIN_ADDRESS);
 	struct vtp_args *uap=args;
 	unsigned long vaddr=uap->vaddr;
 	unsigned long *to_fill=uap->to_fill;
@@ -73,48 +78,48 @@ vtp(struct thread *td, void *args) {
 	////////////////////////////////////////////////////////////////////
 	//pml5e
 	if(la57_flag) {			//5-level paging
-		uprintf("[debug]: %p\n", (void *)PHYS_TO_VIRT( PE_ADDR_MASK(cr3)|(PML5_MASK(vaddr)>>51) ));
 		psentry=*(unsigned long *)\
 			PHYS_TO_VIRT( PE_ADDR_MASK(cr3)|(PML5_MASK(vaddr)>>51) );
-			if(PE_P_FLAG(psentry)) {
+			if(!PE_P_FLAG(psentry)) {
 				return EFAULT; }}
 	else {
 		psentry=cr3; }
 	////////////////////////////////////////////////////////////////////
 	//pml4e
-	uprintf("[debug]: %p\n", (void *)PHYS_TO_VIRT( PE_ADDR_MASK(psentry)|(PML4_MASK(vaddr)>>42) ));
-	return 0; //[debug]
+	uprintf("[debug]: 0x%lx\n", psentry);
+	uprintf("[debug]: 0x%lx\n", PHYS_TO_VIRT( PE_ADDR_MASK(psentry)|(PML4_MASK(vaddr)>>42) ));
 	psentry=*(unsigned long *)\
 		PHYS_TO_VIRT( PE_ADDR_MASK(psentry)|(PML4_MASK(vaddr)>>42) );
-	if(PE_P_FLAG(psentry)) {
+	uprintf("[debug]: 0x%lx\n", psentry);
+	if(!PE_P_FLAG(psentry)) {
 		return EFAULT; }
 	////////////////////////////////////////////////////////////////////
 	//pdpte
-	uprintf("[debug]: %p\n", (void *)PHYS_TO_VIRT( PE_ADDR_MASK(psentry)|(PDPT_MASK(vaddr)>>33) ));
 	psentry=*(unsigned long *)\
 		PHYS_TO_VIRT( PE_ADDR_MASK(psentry)|(PDPT_MASK(vaddr)>>33) );
+	uprintf("[debug]: 0x%lx\n", psentry);
 	if(PE_PS_FLAG(psentry)) {	//1GB page
 		//bits (51 to 30) | bits (29 to 0)
 		paddr=(psentry&0x0ffffc00000000)|(vaddr&0x3fffffff);
 		return copyout(&paddr, to_fill, sizeof(unsigned long)); }
-	if(PE_P_FLAG(psentry)) {
+	if(!PE_P_FLAG(psentry)) {
 		return EFAULT; }
 	////////////////////////////////////////////////////////////////////
 	//pde
-	uprintf("[debug]: %p\n", (void *)PHYS_TO_VIRT( PE_ADDR_MASK(psentry)|(PD_MASK(vaddr)>>24) ));
 	psentry=*(unsigned long *)\
 		PHYS_TO_VIRT( PE_ADDR_MASK(psentry)|(PD_MASK(vaddr)>>24) );
+	uprintf("[debug]: 0x%lx\n", psentry);
 	if(PE_PS_FLAG(psentry)) {	//2MB page
 		//bits (51 to 21) | bits (20 to 0)
 		paddr=(psentry&0x0ffffffffe0000)|(vaddr&0x1ffff);
 		return copyout(&paddr, to_fill, sizeof(unsigned long)); }
-	if(PE_P_FLAG(psentry)) {
+	if(!PE_P_FLAG(psentry)) {
 		return EFAULT; }
 	////////////////////////////////////////////////////////////////////
 	//pte
-	uprintf("[debug]: %p\n", (void *)PHYS_TO_VIRT( PE_ADDR_MASK(psentry)|(PT_MASK(vaddr)>>15) ));
 	psentry=*(unsigned long *)\
 		PHYS_TO_VIRT( PE_ADDR_MASK(psentry)|(PT_MASK(vaddr)>>15) );
+	uprintf("[debug]: 0x%lx\n", psentry);
 	paddr=(psentry&0x0ffffffffff000)|(vaddr&0xfff);
 	return copyout(&paddr, to_fill, sizeof(unsigned long)); }
 	////////////////////////////////////////////////////////////////////

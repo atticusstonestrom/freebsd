fc743530c60c96e5ec39c484eb74652f41fab9ea
atticusstonestrom <atticusstonestrom@yahoo.com>
2020-06-27 19:25:46 -0400

    Update idt_hook.c

--- idt_hook/idt_hook.c
+++ idt_hook/idt_hook.c
@@ -40,7 +40,7 @@ struct idtr_t {
 	unsigned short lim_val;
 	unsigned long addr; }
 	__attribute__((packed))
-	idtr;
+	old_idtr, new_idtr;
 
 __asm__(
 	".global asm_hook;"
@@ -56,62 +56,82 @@ load(struct module *module, int cmd, void *arg) {
 	int error=0;
 	switch(cmd) {
 		case MOD_LOAD:
-			__asm__ __volatile__ (
-				"sidt idtr;"
-				::: "memory");
-			uprintf("[*] idtr dump\n"
+			__asm__ __volatile__ ("sidt old_idtr;");
+			uprintf("[*]  idtr dump\n"
 				"[**] address:\t%p\n"
-				"[**] limit val:\t0x%x\n"
-				"[*] end dump\n\n",
-				(void *)idtr.addr, idtr.lim_val);
-			old_idt=(struct idte_t *)idtr.addr;
+				"[**] lim val:\t0x%x\n"
+				"[*]  end dump\n\n",
+				(void *)old_idtr.addr, old_idtr.lim_val);
+			old_idt=(struct idte_t *)old_idtr.addr;
 			
-			uprintf("[*] allocating new idt table\n"
-				"[**] %d bytes\n",
-				idtr.lim_val+1);
-			new_idt=malloc(idtr.lim_val+1, M_TEMP, M_NOWAIT);
+			uprintf("[*]  intializing new idt\n"
+				"[**] allocating %d bytes\n",
+				old_idtr.lim_val+1);
+			new_idt=malloc(old_idtr.lim_val+1, M_TEMP, M_NOWAIT);
 			if(new_idt==NULL) {
 				uprintf("[*] failed to allocate, returning enomem\n");
 				error=ENOMEM;
 				break; }
-			uprintf("[*] allocation successful: idt @ %p\n", new_idt);
-			memcpy(new_idt, old_idt, idtr.lim_val+1);
+			uprintf("[**] new idt:\t%p\n", new_idt);
+			memcpy(new_idt, old_idt, old_idtr.lim_val+1);
+			uprintf("[**] copied:\t%p\n", old_idt);
+			uprintf("[**] filling new idtr\n");
+			new_idtr.addr=(unsigned long)new_idt;
+			new_idtr.lim_val=old_idtr.lim_val;
+			uprintf("[*]  initialization complete\n\n");
 
-			uprintf("[*] old idt entry %d:\n"
+			uprintf("[*]  old idt entry %d:\n"
 				"[**] addr:\t%p\n"
 				"[**] ist:\t%d\n"
 				"[**] type:\t%d\n"
 				"[**] dpl:\t%d\n"
-				"[**] p:\t%d\n"
-				"[*] end dump\n\n",
+				"[**] p:\t\t%d\n"
+				"[*]  end dump\n\n",
 				ZD_INT, (void *)(\
 				(long)old_idt[ZD_INT].offset_0_15|((long)old_idt[ZD_INT].offset_16_31<<16)|((long)old_idt[ZD_INT].offset_32_63<<32)),
 				old_idt[ZD_INT].ist, old_idt[ZD_INT].type, old_idt[ZD_INT].dpl, old_idt[ZD_INT].p);
 			//check for present flag
 
-			new_idt[ZD_INT].offset_0_15=((unsigned long)(&asm_hook))&0xffff;
+			/*new_idt[ZD_INT].offset_0_15=((unsigned long)(&asm_hook))&0xffff;
 			new_idt[ZD_INT].offset_16_31=((unsigned long)(&asm_hook)>>16)&0xffff;
-			new_idt[ZD_INT].offset_32_63=((unsigned long)(&asm_hook)>>32)&0xffffffff;
-			uprintf("[*] new idt entry %d:\n"
+			new_idt[ZD_INT].offset_32_63=((unsigned long)(&asm_hook)>>32)&0xffffffff;*/
+			uprintf("[*]  new idt entry %d:\n"
 				"[**] addr:\t%p\n"
 				"[**] ist:\t%d\n"
 				"[**] type:\t%d\n"
 				"[**] dpl:\t%d\n"
-				"[**] p:\t%d\n"
-				"[*] end dump\n\n",
+				"[**] p:\t\t%d\n"
+				"[*]  end dump\n\n",
 				ZD_INT, (void *)(\
 				(long)new_idt[ZD_INT].offset_0_15|((long)new_idt[ZD_INT].offset_16_31<<16)|((long)new_idt[ZD_INT].offset_32_63<<32)),
 				new_idt[ZD_INT].ist, new_idt[ZD_INT].type, new_idt[ZD_INT].dpl, new_idt[ZD_INT].p);
-			/*uprintf("\n\t\"");
-			for(int i=3072; i<4096; i++) {
-				uprintf("\\x%02x", *(unsigned char *)(idte_offset+i));
-				if(!( (i&0x0f)^0x0f )) {
-					uprintf("\"\n\t\""); }}*/
-			//lock should go here?
+				
+			uprintf("[*]  loading new idt\n"
+				"[**] address:\t%p\n",
+				new_idt);
+			__asm__ __volatile__("lidt new_idtr");
+			/*int j;
+			for(int i=0; i<64; i++) {
+				uprintf("\n\t");
+				for(j=0; j<8; j++) {
+					uprintf("%02x ", *((unsigned char *)(old_idt)+i*8+j)); }
+				uprintf("   ");
+				for(j=0; j<8; j++) {
+					uprintf("%02x ", *((unsigned char *)(new_idt)+i*8+j)); }}*/
+			uprintf("[*]  done\n\n");
 			break;
 		case MOD_UNLOAD:
-			//lock here?
+			uprintf("[*]  loading old idt\n"
+				"[**] address:\t%p\n",
+				old_idt);
+			__asm__ __volatile__("lidt old_idtr");
+			uprintf("[*]  done\n\n");
+		
+			uprintf("[*]  freeing new idt\n"
+				"[**] address:\t%p\n",
+				new_idt);
 			free(new_idt, M_TEMP);
+			uprintf("[*]  done\n\n");
 			break;
 		default:
 			error=EOPNOTSUPP;

07ec867d69f50ef9c412b8068c6cd2153478d17d
atticusstonestrom <atticusstonestrom@yahoo.com>
2020-07-05 23:07:20 -0400

    Update vtp_test.c

--- vtp/linux/vtp_test.c
+++ vtp/linux/vtp_test.c
@@ -5,12 +5,12 @@
 #define VTP_NUM 335
 
 int main() {
-	int x=0;
-	unsigned long vaddr=(unsigned long)&x;
+	int r=0;
+	unsigned long vaddr=(unsigned long)&r;
 	unsigned long paddr=0;
 
-	__asm__ __volatile__("syscall":"=a"(paddr):"a"(VTP_NUM), "D"(vaddr));
-	if(!paddr) {
+	__asm__ __volatile__("syscall":"=a"(r):"a"(VTP_NUM), "D"(vaddr), "S"(&paddr));
+	if(r) {
 		perror("fatal in syscall");
 		exit(-1); }
 	printf("virtual address:	%p\n"

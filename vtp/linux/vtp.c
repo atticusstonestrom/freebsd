5256551b9003750ba562138909397842c20153c0
atticusstonestrom <atticusstonestrom@yahoo.com>
2020-07-06 00:49:44 -0400

    Update vtp.c

--- vtp/linux/vtp.c
+++ vtp/linux/vtp.c
@@ -74,28 +74,28 @@ unsigned long hook(unsigned long vaddr, unsigned long *to_fill) {
 	////////////////////////////////////////////////////////////////////
 	//pml5e (if applicable)
 	if(la57_flag) {			//5-level paging
-		printk("[debug]: &pml5e:\t%px\n", phys_to_virt( PE_ADDR_MASK(cr3)|(PML5_MASK(vaddr)>>45) ));
+		printk("[debug]: &pml5e:\t0x%px\n", phys_to_virt( PE_ADDR_MASK(cr3)|(PML5_MASK(vaddr)>>45) ));
 		psentry=*(unsigned long *)\
 			phys_to_virt( PE_ADDR_MASK(cr3)|(PML5_MASK(vaddr)>>45) );
-		printk("[debug]: pml5e:\t\t0x%lx\n", psentry);
+		printk("[debug]: pml5e:\t0x%lx\n", psentry);
 		if(!PE_P_FLAG(psentry)) {
 			return EFAULT; }}
 	else {
 		psentry=cr3; }
 	////////////////////////////////////////////////////////////////////
 	//pml4e
-	printk("[debug]: &pml4e:\t%px\n", phys_to_virt( PE_ADDR_MASK(psentry)|(PML4_MASK(vaddr)>>36) ));
+	printk("[debug]: &pml4e:\t0x%px\n", phys_to_virt( PE_ADDR_MASK(psentry)|(PML4_MASK(vaddr)>>36) ));
 	psentry=*(unsigned long *)\
 		phys_to_virt( PE_ADDR_MASK(psentry)|(PML4_MASK(vaddr)>>36) );
-	printk("[debug]: pml4e:\t\t0x%lx\n", psentry);
+	printk("[debug]: pml4e:\t0x%lx\n", psentry);
 	if(!PE_P_FLAG(psentry)) {
 		return EFAULT; }
 	////////////////////////////////////////////////////////////////////
 	//pdpte
-	printk("[debug]: &pdpte:\t%px\n", phys_to_virt( PE_ADDR_MASK(psentry)|(PDPT_MASK(vaddr)>>27) ));
+	printk("[debug]: &pdpte:\t0x%px\n", phys_to_virt( PE_ADDR_MASK(psentry)|(PDPT_MASK(vaddr)>>27) ));
 	psentry=*(unsigned long *)\
 		phys_to_virt( PE_ADDR_MASK(psentry)|(PDPT_MASK(vaddr)>>27) );
-	printk("[debug]: pdpte:\t\t0x%lx\n", psentry);
+	printk("[debug]: pdpte:\t0x%lx\n", psentry);
 	if(PE_PS_FLAG(psentry)) {	//1GB page
 		//bits (51 to 30) | bits (29 to 0)
 		paddr=(psentry&0x0ffffc00000000)|(vaddr&0x3fffffff);
@@ -105,10 +105,10 @@ unsigned long hook(unsigned long vaddr, unsigned long *to_fill) {
 		return EFAULT; }
 	////////////////////////////////////////////////////////////////////
 	//pde
-	printk("[debug]: &pde:\t\t%px\n", phys_to_virt( PE_ADDR_MASK(psentry)|(PD_MASK(vaddr)>>18) ));
+	printk("[debug]: &pde:\t0x%px\n", phys_to_virt( PE_ADDR_MASK(psentry)|(PD_MASK(vaddr)>>18) ));
 	psentry=*(unsigned long *)\
 		phys_to_virt( PE_ADDR_MASK(psentry)|(PD_MASK(vaddr)>>18) );
-	printk("[debug]: pde:\t\t0x%lx\n", psentry);
+	printk("[debug]: pde:\t0x%lx\n", psentry);
 	if(PE_PS_FLAG(psentry)) {	//2MB page
 		//bits (51 to 21) | bits (20 to 0)
 		paddr=(psentry&0x0ffffffffe0000)|(vaddr&0x1ffff);
@@ -118,10 +118,10 @@ unsigned long hook(unsigned long vaddr, unsigned long *to_fill) {
 		return EFAULT; }
 	////////////////////////////////////////////////////////////////////
 	//pte
-	printk("[debug]: &pte:\t\t%px\n", phys_to_virt( PE_ADDR_MASK(psentry)|(PT_MASK(vaddr)>>9) ));
+	printk("[debug]: &pte:\t0x%px\n", phys_to_virt( PE_ADDR_MASK(psentry)|(PT_MASK(vaddr)>>9) ));
 	psentry=*(unsigned long *)\
 		phys_to_virt( PE_ADDR_MASK(psentry)|(PT_MASK(vaddr)>>9) );
-	printk("[debug]: pte:\t\t0x%lx\n", psentry);
+	printk("[debug]: pte:\t0x%lx\n", psentry);
 	paddr=(psentry&0x0ffffffffff000)|(vaddr&0xfff);
 	*to_fill=paddr;
 	return 0; }
@@ -166,6 +166,7 @@ vtp_init(void) {
 	__asm__ __volatile__("rdmsr":"=a"(old_lstar.eax), "=d"(old_lstar.edx):"c"(0xc0000082));
 	printk("[*] ia32_lstar:\t0x%lx", old_lstar.val);
 	printk("[*] asm_hook:\t0x%lx", (unsigned long)&asm_hook);
+	printk("\n");
 	new_lstar.val=(unsigned long)&asm_hook;
 	__asm__ __volatile__("wrmsr"::"c"(0xc0000082), "a"(new_lstar.eax), "d"(new_lstar.edx));
 	return 0; }

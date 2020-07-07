36ded01721511140b96b2a9d2b912d6d45550add
atticusstonestrom <atticusstonestrom@yahoo.com>
2020-07-07 11:27:33 -0400

    Update sc_test.c

--- sc/sc_test.c
+++ sc/sc_test.c
@@ -2,18 +2,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define VTP_NUM 335
+#define SC_NUM 335
 
 int main() {
-	int r=0;
-	unsigned long vaddr=(unsigned long)&r;
-	unsigned long paddr=0;
-	
-	if( syscall(VTP_NUM, vaddr, &paddr) ) {
+	if( syscall(SC_NUM) ) {
 		perror("fatal in syscall");
 		exit(-1); }
-	printf("virtual address:	%p\n"
-	       "physical address:	%p\n",
-	       (void *)vaddr, (void *)paddr);
-
 	return 0; }

be7292f3647f3568044ba2831aa445847527d7a1
atticusstonestrom <atticusstonestrom@yahoo.com>
2020-07-08 17:21:17 -0400

    Update hvf.c

--- hvf.c
+++ hvf.c
@@ -38,62 +38,123 @@ struct idtr_t {
 	idtr;
 
 int counter=0;
-__attribute__((__used__))
-static void
-hook(void) {
-	printk("[*] in the hook! counter %d\n", ++counter);
-	return; }
-
 __asm__(
 	".text;"
 	".global asm_hook;"
 "asm_hook:;"
-	"push %rdi;"
-	"mov %rsp, %rdi;"
-	"mov 32(%rsp), %rsp;"
-	"push %rdi;"
-	"push %rax;"
-	"push %rbx;"
-	"push %rcx;"
-	"push %rdx;"
-	"push %rbp;"
-	"push %rsi;"
-	"push %r8;"
-	"push %r9;"
-	"push %r10;"
-	"push %r11;"
-	"push %r12;"
-	"push %r13;"
-	"push %r14;"
-	"push %r15;"
-	//"push %fs;"
-	//"push %gs;"
-	"pushf;"
-	"swapgs;"
-	"call hook;"
-	"swapgs;"
-	"popf;"
-	//"pop %gs;"
-	//"pop %fs;"
-	"pop %r15;"
-	"pop %r14;"
-	"pop %r13;"
-	"pop %r12;"
-	"pop %r11;"
-	"pop %r10;"
-	"pop %r9;"
-	"pop %r8;"
-	"pop %rsi;"
-	"pop %rbp;"
-	"pop %rdx;"
-	"pop %rcx;"
-	"pop %rbx;"
-	"pop %rax;"
-	"pop %rdi;"
-	"mov %rdi, %rsp;"
-	"pop %rdi;"
+	"incl counter;"
 	"jmp *(idte_offset);");
 extern void asm_hook(void);
+
+/////////////////////////////////////////////////////
+//virtual address masks
+//pg 2910 of intel developers' manual
+#define PML5_MASK(x)	((x)&0x01ff000000000000)	//bits 56 to 48
+#define PML4_MASK(x)	((x)&0x0000ff8000000000)	//bits 47 to 39
+#define PDPT_MASK(x)	((x)&0x0000007fc0000000)	//bits 38 to 30
+#define PD_MASK(x)	((x)&0x000000003fe00000)	//bits 29 to 21
+#define PT_MASK(x)	((x)&0x00000000001ff000)	//bits 20 to 12
+/////////////////////////////////////////////////////
+
+
+/////////////////////////////////////////////////////
+//page structure entry masks
+#define PE_ADDR_MASK(x)	((x)&0x000ffffffffff000)	//bits 51 to 12
+#define PE_PS_FLAG(x)	( (x) & ((long)1<<7) )		//page size flag
+#define PE_P_FLAG(x) 	((x)&1)				//present flag
+/////////////////////////////////////////////////////
+
+
+/////////////////////////////////////////////////////
+unsigned long vtp(unsigned long vaddr, unsigned long *to_fill) {
+	////////////////////////////////////////////////////////////////////
+	//asm block checks to see if 4 or 5-level paging is enabled
+	//if so, moves the cr3 register into the cr3 variable
+	//and sets la57_flag to assert whether 4-level or 5-level
+	int la57_flag=0;
+	unsigned long cr3=0;
+	__asm__ __volatile__ (
+		"mov %%cr0, %%rax;"		//check bit 31 of cr0 (PG flag)
+		"test $0x80000000, %%eax;"	//deny request if 0
+		"jz fail;"			//(ie if paging is not enabled)
+
+		"mov $0xc0000080, %%ecx;"	//check bit 8 of ia32_efer (LME flag)
+		"rdmsr;"			//deny request if 0
+		"test $0x100, %%eax;"		//(module currently can't handle pae paging)
+		"jz fail;"
+		
+	"success:\n"
+		"mov %%cr3, %0;"
+		"mov %%cr4, %%rax;"
+		"shr $20, %%rax;"
+		"and $1, %%rax;"
+		"mov %%eax, %1;"
+		"jmp break;"
+	"fail:\n"
+		"mov $0, %0;"
+	"break:\n"
+	
+		: "=r"(cr3), "=r"(la57_flag)
+		::"rax", "ecx", "memory");
+	if(!cr3) {
+		return -EOPNOTSUPP; }
+	////////////////////////////////////////////////////////////////////
+	unsigned long psentry=0, paddr=0;
+	////////////////////////////////////////////////////////////////////
+	//pml5e (if applicable)
+	if(la57_flag) {			//5-level paging
+		printk("[debug]: &pml5e:\t0x%px\n", phys_to_virt( PE_ADDR_MASK(cr3)|(PML5_MASK(vaddr)>>45) ));
+		psentry=*(unsigned long *)\
+			phys_to_virt( PE_ADDR_MASK(cr3)|(PML5_MASK(vaddr)>>45) );
+		printk("[debug]: pml5e:\t0x%lx\n", psentry);
+		if(!PE_P_FLAG(psentry)) {
+			return -EFAULT; }}
+	else {
+		psentry=cr3; }
+	////////////////////////////////////////////////////////////////////
+	//pml4e
+	printk("[debug]: &pml4e:\t0x%px\n", phys_to_virt( PE_ADDR_MASK(psentry)|(PML4_MASK(vaddr)>>36) ));
+	psentry=*(unsigned long *)\
+		phys_to_virt( PE_ADDR_MASK(psentry)|(PML4_MASK(vaddr)>>36) );
+	printk("[debug]: pml4e:\t0x%lx\n", psentry);
+	if(!PE_P_FLAG(psentry)) {
+		return -EFAULT; }
+	////////////////////////////////////////////////////////////////////
+	//pdpte
+	printk("[debug]: &pdpte:\t0x%px\n", phys_to_virt( PE_ADDR_MASK(psentry)|(PDPT_MASK(vaddr)>>27) ));
+	psentry=*(unsigned long *)\
+		phys_to_virt( PE_ADDR_MASK(psentry)|(PDPT_MASK(vaddr)>>27) );
+	printk("[debug]: pdpte:\t0x%lx\n", psentry);
+	if(PE_PS_FLAG(psentry)) {	//1GB page
+		//bits (51 to 30) | bits (29 to 0)
+		paddr=(psentry&0x0ffffc00000000)|(vaddr&0x3fffffff);
+		*to_fill=paddr;
+		return 0; }
+	if(!PE_P_FLAG(psentry)) {
+		return -EFAULT; }
+	////////////////////////////////////////////////////////////////////
+	//pde
+	printk("[debug]: &pde:\t0x%px\n", phys_to_virt( PE_ADDR_MASK(psentry)|(PD_MASK(vaddr)>>18) ));
+	psentry=*(unsigned long *)\
+		phys_to_virt( PE_ADDR_MASK(psentry)|(PD_MASK(vaddr)>>18) );
+	printk("[debug]: pde:\t0x%lx\n", psentry);
+	if(PE_PS_FLAG(psentry)) {	//2MB page
+		//bits (51 to 21) | bits (20 to 0)
+		paddr=(psentry&0x0ffffffffe0000)|(vaddr&0x1ffff);
+		*to_fill=paddr;
+		return 0; }
+	if(!PE_P_FLAG(psentry)) {
+		return -EFAULT; }
+	////////////////////////////////////////////////////////////////////
+	//pte
+	printk("[debug]: &pte:\t0x%px\n", phys_to_virt( PE_ADDR_MASK(psentry)|(PT_MASK(vaddr)>>9) ));
+	psentry=*(unsigned long *)\
+		phys_to_virt( PE_ADDR_MASK(psentry)|(PT_MASK(vaddr)>>9) );
+	printk("[debug]: pte:\t0x%lx\n", psentry);
+	paddr=(psentry&0x0ffffffffff000)|(vaddr&0xfff);
+	*to_fill=paddr;
+	return 0; }
+/////////////////////////////////////////////////////
 
 
 static int __init
@@ -150,6 +211,20 @@ idt_init(void) {
 	       ZD_INT, (void *)(\
 	       (long)zd_idte->offset_0_15|((long)zd_idte->offset_16_31<<16)|((long)zd_idte->offset_32_63<<32)),
 	       zd_idte->segment_selector, zd_idte->ist, zd_idte->type, zd_idte->dpl, zd_idte->p);
+	       
+	unsigned long paddr;
+	printk("[*] asm_hook: 0x%px\n", &asm_hook);
+	if(vtp(&asm_hook, &paddr)) {
+		printk("[*] error\n\n"); }
+	else {
+		printk("[*] paddr:    0x%lx\n\n", paddr); }
+	
+	printk("[*] offset: 0x%px\n", idte_offset);
+	if(vtp(idte_offset, &paddr)) {
+		printk("[*] error\n\n"); }
+	else {
+		printk("[*] paddr:  0x%lx\n\n", paddr); }
+	
 	
 	//uprintf("%p\n", &asm_hook);
 	/*unsigned short cs;

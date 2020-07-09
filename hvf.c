7ebddbc8341df4d1ac105234e84f4185f5e74a36
atticusstonestrom <atticusstonestrom@yahoo.com>
2020-07-09 14:08:00 -0400

    Update hvf.c

--- hvf.c
+++ hvf.c
@@ -32,76 +32,6 @@ __asm__(
 	"jmp *(idte_offset);");
 extern void asm_hook(void);
 
-
-/////////////////////////////////////////////////////
-unsigned long print_vtp(unsigned long vaddr, unsigned long *to_fill) {
-	////////////////////////////////////////////////////////////////////
-	//asm block checks to see if 4 or 5-level paging is enabled
-	//if so, moves the cr3 register into the cr3 variable
-	//and sets la57_flag to assert whether 4-level or 5-level
-	int la57_flag=0;
-	unsigned long cr3=0;
-	__asm__ __volatile__ ("mov %%cr3, %0;":"=r"(cr3));
-	if(!cr3) {
-		return -EOPNOTSUPP; }
-	////////////////////////////////////////////////////////////////////
-	unsigned long psentry=0, paddr=0;
-	////////////////////////////////////////////////////////////////////
-	//pml5e (if applicable)
-	if(la57_flag) {			//5-level paging
-		printk("[debug]: &pml5e:\t0x%px\n", phys_to_virt( PE_ADDR_MASK(cr3)|(PML5_MASK(vaddr)>>45) ));
-		psentry=*(unsigned long *)\
-			phys_to_virt( PE_ADDR_MASK(cr3)|(PML5_MASK(vaddr)>>45) );
-		printk("[debug]: pml5e:\t0x%lx\n", psentry);
-		if(!PE_P_FLAG(psentry)) {
-			return -EFAULT; }}
-	else {
-		psentry=cr3; }
-	////////////////////////////////////////////////////////////////////
-	//pml4e
-	printk("[debug]: &pml4e:\t0x%px\n", phys_to_virt( PE_ADDR_MASK(psentry)|(PML4_MASK(vaddr)>>36) ));
-	psentry=*(unsigned long *)\
-		phys_to_virt( PE_ADDR_MASK(psentry)|(PML4_MASK(vaddr)>>36) );
-	printk("[debug]: pml4e:\t0x%lx\n", psentry);
-	if(!PE_P_FLAG(psentry)) {
-		return -EFAULT; }
-	////////////////////////////////////////////////////////////////////
-	//pdpte
-	printk("[debug]: &pdpte:\t0x%px\n", phys_to_virt( PE_ADDR_MASK(psentry)|(PDPT_MASK(vaddr)>>27) ));
-	psentry=*(unsigned long *)\
-		phys_to_virt( PE_ADDR_MASK(psentry)|(PDPT_MASK(vaddr)>>27) );
-	printk("[debug]: pdpte:\t0x%lx\n", psentry);
-	if(PE_PS_FLAG(psentry)) {	//1GB page
-		//bits (51 to 30) | bits (29 to 0)
-		paddr=(psentry&0x0ffffc00000000)|(vaddr&0x3fffffff);
-		*to_fill=paddr;
-		return 0; }
-	if(!PE_P_FLAG(psentry)) {
-		return -EFAULT; }
-	////////////////////////////////////////////////////////////////////
-	//pde
-	printk("[debug]: &pde:\t0x%px\n", phys_to_virt( PE_ADDR_MASK(psentry)|(PD_MASK(vaddr)>>18) ));
-	psentry=*(unsigned long *)\
-		phys_to_virt( PE_ADDR_MASK(psentry)|(PD_MASK(vaddr)>>18) );
-	printk("[debug]: pde:\t0x%lx\n", psentry);
-	if(PE_PS_FLAG(psentry)) {	//2MB page
-		//bits (51 to 21) | bits (20 to 0)
-		paddr=(psentry&0x0ffffffffe0000)|(vaddr&0x1ffff);
-		*to_fill=paddr;
-		return 0; }
-	if(!PE_P_FLAG(psentry)) {
-		return -EFAULT; }
-	////////////////////////////////////////////////////////////////////
-	//pte
-	printk("[debug]: &pte:\t0x%px\n", phys_to_virt( PE_ADDR_MASK(psentry)|(PT_MASK(vaddr)>>9) ));
-	psentry=*(unsigned long *)\
-		phys_to_virt( PE_ADDR_MASK(psentry)|(PT_MASK(vaddr)>>9) );
-	printk("[debug]: pte:\t0x%lx\n", psentry);
-	paddr=(psentry&0x0ffffffffff000)|(vaddr&0xfff);
-	*to_fill=paddr;
-	return 0; }
-/////////////////////////////////////////////////////
-
 static void
 print_vtp_s(struct vtp_t *vtp_p) {
 	if(vtp_p->pml5e_p) {
@@ -171,11 +101,6 @@ idt_init(void) {
 	       
 	unsigned long paddr;
 	struct vtp_t vtp_s={0};
-	printk("[*] asm_hook: 0x%px\n", &asm_hook);
-	if(print_vtp((unsigned long)&asm_hook, &paddr)) {
-		printk("[*] error\n\n"); }
-	else {
-		printk("[*] paddr: 0x%lx\n\n", paddr); }
 	
 	vtp_s=(struct vtp_t){0};
 	if(vtp((unsigned long)&asm_hook, &paddr, &vtp_s)) {
@@ -185,14 +110,7 @@ idt_init(void) {
 		printk("[*] asm_hook: 0x%px\n", &asm_hook);
 		print_vtp_s(&vtp_s);
 		printk("[*] paddr: 0x%lx\n\n", paddr); }
-	
-	printk("[*] offset: 0x%lx\n", idte_offset);
-	if(print_vtp(idte_offset, &paddr)) {
-		printk("[*] error\n\n"); }
-	else {
-		printk("[*] paddr: 0x%lx\n\n", paddr); }
 
-	//printk("[DEBUG]: vtp_s @ 0x%px\n", &vtp_s);
 	vtp_s=(struct vtp_t){0};
 	if(vtp(idte_offset, &paddr, &vtp_s)) {
 		printk("[*] error\n\n"); }
